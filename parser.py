fca01f5f0ff591fe261aed6485f9906354de6523
Daniel Hochmuth <sleepy.locust@gmail.com>
2018-06-14 13:19:01 +0200

    no comma on the last line

--- parser.py
+++ parser.py
@@ -19,14 +19,30 @@ for file in file_list:
     # Produce the SQL statement
     print('PRINT \''+file[:-4]+'\'')
     print('SELECT')
+    
     # Main conditional. Fields.dates/fields.decimals contain the date/decimal fields to convert.
     for column in column_names:
-        if column in (fields.dates):
-            print('CASE ['+column+'] WHEN \'00000000\' THEN NULL ELSE CONVERT(DATE, ['+column+'], 101) END AS ['+file[:-4]+'_'+column+'],')
-        elif column in (fields.decimals):
-            print('CASE WHEN CHARINDEX(\'-\', ['+column+']) > 0 THEN CONVERT(DECIMAL(15,2), SUBSTRING(['+column+'], CHARINDEX(\'-\', ['+column+']), LEN('+column+')) + SUBSTRING(['+column+'], 0, CHARINDEX(\'-\', ['+column+']))) ELSE CONVERT(DECIMAL(15,2), ['+column+']) END AS ['+file[:-4]+'_'+column+'],')
+        
+        # If it's the last column, there shouldn't be a trailing comma.
+        if column == column_names[-1]:
+            if column in (fields.dates):
+                print('CASE ['+column+'] WHEN \'00000000\' THEN NULL ELSE CONVERT(DATE, ['+column+'], 101) END AS ['+file[:-4]+'_'+column+']')
+                break
+            elif column in (fields.decimals):
+                print('CASE WHEN CHARINDEX(\'-\', ['+column+']) > 0 THEN CONVERT(DECIMAL(15,2), SUBSTRING(['+column+'], CHARINDEX(\'-\', ['+column+']), LEN('+column+')) + SUBSTRING(['+column+'], 0, CHARINDEX(\'-\', ['+column+']))) ELSE CONVERT(DECIMAL(15,2), ['+column+']) END AS ['+file[:-4]+'_'+column+']')
+                break
+            else:
+                print('LTRIM(RTRIM(['+column+'])) AS ['+file[:-4]+'_'+column+']')
+                break
+        # All other columns except the last one:    
         else:
-            print('LTRIM(RTRIM(['+column+'])) AS ['+file[:-4]+'_'+column+'],')
+            if column in (fields.dates):
+                print('CASE ['+column+'] WHEN \'00000000\' THEN NULL ELSE CONVERT(DATE, ['+column+'], 101) END AS ['+file[:-4]+'_'+column+'],')
+            elif column in (fields.decimals):
+                print('CASE WHEN CHARINDEX(\'-\', ['+column+']) > 0 THEN CONVERT(DECIMAL(15,2), SUBSTRING(['+column+'], CHARINDEX(\'-\', ['+column+']), LEN('+column+')) + SUBSTRING(['+column+'], 0, CHARINDEX(\'-\', ['+column+']))) ELSE CONVERT(DECIMAL(15,2), ['+column+']) END AS ['+file[:-4]+'_'+column+'],')
+            else:
+                print('LTRIM(RTRIM(['+column+'])) AS ['+file[:-4]+'_'+column+'],')
+                
     print('INTO ['+file[:-4]+']')    
     print('FROM [00_'+file[:-4]+']')
     print('\n')
@@ -35,7 +51,7 @@ for file in file_list:
     temp_file.close()
         
         # TO DO:
-        # The last comma of the select statement needs to be removed.
+        # Check different separators.
         
         
         
