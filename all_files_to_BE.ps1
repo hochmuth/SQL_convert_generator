7c921449266d7c07edc37c2d0f2b0900d124b24f
Daniel Hochmuth <sleepy.locust@gmail.com>
2018-11-19 09:03:20 +0100

    clean-up

--- all_files_to_BE.ps1
+++
@@ -1,6 +0,0 @@
-$files = [IO.Directory]::GetFiles("c:\Users\es579dx\Desktop\temp DATA\Python_Parser\sample_data\")
-foreach($file in $files) 
-{     
-    $content = get-content -path $file
-    $content | out-file $file -encoding BigEndianUnicode   
-}
