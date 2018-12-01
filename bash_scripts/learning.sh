892a8b7dbe8f6bb4c6d9dd2664a4b579504a0978
hockmood <sleepy.locust@gmail.com>
2018-12-01 15:12:06 +0100

    learning bash

--- bash_scripts/learning.sh
+++ bash_scripts/learning.sh
@@ -4,6 +4,24 @@ printf "The first program you write is always '%s %s'!\n" hello world
 
 for f in *.txt
 do
-	tr -d '\r' < $f >> test_output.txt
+	tr -d '\r' < $f > test_output.txt
 done
 
+# removes the carriage return, then sorts, then saves into a new file
+#for f in *.txt
+#do
+#	tr -d '\r' < $f | sort > unix-file.txt
+#done
+
+# Tip - when constructing pipelines, the amount of data should be reduced at each stage. Improves the efficiency. 
+# For example, first use grep to select lines you want, and only then apply sort.
+
+echo $PATH
+
+# Find user ---- see if user named by first argument is logged in
+#who | grep $1
+
+# Execution tracing works like this:
+set -x		#turn tracing on
+who | wc -l
+set +x		# turn tracing off
