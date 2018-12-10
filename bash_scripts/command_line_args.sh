6f2858ce6baaaefde7fe01f045ebe6ceb6b9a2b9
Daniel Hochmuth <sleepy.locust@gmail.com>
2018-12-10 16:47:20 +0100

    Update command_line_args.sh

--- bash_scripts/command_line_args.sh
+++ bash_scripts/command_line_args.sh
@@ -17,19 +17,62 @@ then
 	do
 		echo i is $i
 	done
+	
 	for i in "$*"
 	do
 		echo i is $i
 	done
+	
 	for i in "$@"
 	do echo i is $i
 	done
 	shift # lop off the first arg
 	echo there are now $# arguments
+	
 	for i in "$@"
 	do 
 		echo i is $i
 	done
 fi 
+
+# LOOPS
+<< --COMMENT--
+for i in *.txt
+do
+	echo $i
+	mv $i $i.old # create backups
+	sed 's/Atlanta/&, the capital of the South/' < $i.old > $i
+done
+--COMMENT--
+
+pattern=...
+while [ -n "$string" ]
+do
+	string=${string%$pattern}
+	echo $string
+done
+
+<< --COMMENT--
+printf "Enter username: "
+read user
+while true
+do
+	if who | grep "$user" > /dev/null
+	then
+		echo 'no, sorry'
+		break
+	fi
+	
+	echo 'yeah'
+	break
+done
+--COMMENT--
+
+
+while IFS=: read user pass uid gid fullname homedir shell
+do
+	continue	
+done < /etc/passwd # needs to be here (after the loop) so that the file is opened only once
+
 
 
