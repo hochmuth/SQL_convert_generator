c31cab3782304fbf08ae1e2762ecb2d125534c4b
Daniel Hochmuth <sleepy.locust@gmail.com>
2018-12-19 10:32:55 +0100

    trim_SAP_tables converts encoding

--- tools/trim_SAP_tables.sh
+++ tools/trim_SAP_tables.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+extension='txt'
+
 if [[ $# > 1 ]]; then
 	echo "Usage: ./trimAndClean.sh [path/to/files/]"
 	exit
@@ -10,15 +13,37 @@ else
 	DIRECTORY="${1}"
 fi
 
-if [ ! -d "$DIRECTORY/converted" ]; then
-	mkdir "$DIRECTORY/converted"
+if [ -d "$DIRECTORY/01_trimmed" ] || [ -d "$DIRECTORY/02_pipes" ] || [ -d "$DIRECTORY/03_enc" ] ; then
+	echo "Folder '01_trimmed' already exists."
+	exit 1
 fi
-shopt -s nullglob
-for f in "${DIRECTORY}/"*.txt
 
+if [ ! -d "$DIRECTORY/01_trimmed" ] || [ ! -d "$DIRECTORY/02_pipes" ] || [ ! -d "$DIRECTORY/03_enc" ] ; then
+	mkdir "$DIRECTORY/01_trimmed"
+	mkdir "$DIRECTORY/02_pipes"
+	mkdir "$DIRECTORY/03_enc"
+fi
+
+shopt -s nullglob
+
+# First run for removing the white spaces and trailing 02_pipes
+for f in "${DIRECTORY}/"*."${extension}"
 do
 	echo "Processing $(basename "$f") file..."
 	# posledny regex je sporny, odstranuje znak na zaciatku + pipu (nemusi to fungovat spravne)
-	sed -e '/^[^|]/d' -e 's/\s*|/|/g' -e 's/|\s*/|/g' -e 's/|$//' -e 's/^.|//' "${f}"  > "${DIRECTORY}/converted/$(basename "$f")"
-	
+	sed -e '/^[^|]/d' -e 's/\s*|/|/g' -e 's/|\s*/|/g' -e 's/|$//' -e 's/^.|//' "${f}"  > "${DIRECTORY}/01_trimmed/$(basename "$f")"	
 done 
+
+# Second run for removing the leading 02_pipes
+for f in "${DIRECTORY}/"01_trimmed/*."${extension}"
+do
+	echo "Removing leadin 02_pipes of $(basename "$f") file..."
+	sed -e 's/^|//' "${f}"  > "${DIRECTORY}/02_pipes/$(basename "$f")"
+done
+
+# Third run for converting to UTF-16
+for f in "${DIRECTORY}/"02_pipes/*."${extension}"
+do
+	echo "Converting encoding of $(basename "$f") file..."
+	iconv -f utf-8 -t utf-16 "${f}" > "${DIRECTORY}/03_enc/$(basename "$f")"
+done
