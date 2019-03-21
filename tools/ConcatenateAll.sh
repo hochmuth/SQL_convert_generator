8c5f7a279a2d0ef370481558706b5a7828d84996
Daniel Hochmuth <sleepy.locust@gmail.com>
2019-03-21 16:58:20 +0100

    ideas from the net

--- tools/ConcatenateAll.sh
+++ tools/ConcatenateAll.sh
@@ -11,3 +11,19 @@
 # For all files:
 #	Open the file, remove the header, and copy (append) the rest into the output file
 
+
+
+# Ideas
+# First write the header (first line), then everything else 
+head -1 file.txt > all.txt && tail -n +2 -q >> all.txt
+
+# Or
+head -n1 file1.txt > combined.txt
+for fname in *.txt
+do
+	    tail -n+2 $fname >> combined.txt
+done
+
+# Store all files in an array; write the header from the first one; for all the rest append all expect the header
+
+array=( *.txt );head -1 ${array[0]} > all.txt; tail -n +2 -q ${array[@]:0} >> all.txt
