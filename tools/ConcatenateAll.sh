4336c6d4990c346c1ad30e6088418116f45ac95d
Daniel Hochmuth <sleepy.locust@gmail.com>
2019-03-21 16:49:26 +0100

    ConcatenateAll started

---
+++ tools/ConcatenateAll.sh
@@ -0,0 +1,13 @@
+# Should take all text files in a folder and concatenate them into one, 
+# while maintaining only one header (from the first file).
+# Arguments should be as follows: 
+#	directory
+#	extension
+#	output file name
+
+# Logic
+# Open the first file, read the first line (header) and store inside a string
+# Write the header into the output file (?)
+# For all files:
+#	Open the file, remove the header, and copy (append) the rest into the output file
+
