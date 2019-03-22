f02758a9d9e380d1b90a6e25388c0a6a1cd28a30
Daniel Hochmuth <sleepy.locust@gmail.com>
2019-03-22 10:06:54 +0100

    initial test ok

--- tools/ConcatenateAll.sh
+++ tools/ConcatenateAll.sh
@@ -2,33 +2,30 @@
 # while maintaining only one header (from the first file).
 # Arguments should be as follows: 
 #	directory
-#	extension
 #	output file name
 
-# Logic
-# Open the first file, read the first line (header) and store inside a string
-# Write the header into the output file (?)
-# For all files:
-#	Open the file, remove the header, and copy (append) the rest into the output file
-
-
-# Should probably create a new folder to store the newly created file.
-
 # Ideas
-# First write the header (first line), then everything else 
-# head -1 file.txt > all.txt && tail -n +2 -q >> all.txt
-
-# Or
+#
 # head -n1 file1.txt > combined.txt
 # for fname in *.txt
 # do
 # 	    tail -n+2 $fname >> combined.txt
 # done
 
-# Store all files in an array; write the header from the first one; for all the rest append all expect the header
+# Parameters
+extension='txt'
 
-# Arguments
-extension='csv'
+# Deal with arguments
+if [[ $# = 1 ]] || [[ $# > 2 ]]; then
+	echo "Usage: ./ConcatenateAll.sh (directory out_file_name)"
+	exit 2
+fi
+
+if [[ $# = 0 ]]; then
+	out_file="out_file.${extension}"
+else
+	out_file="${2}"
+fi
 
 if [[ $# = 0 ]]; then
 	directory="."
@@ -36,9 +33,8 @@ else
 	directory="${1}"
 fi
 
-
-# Create necessary folder
-if [-d "$directory/joined_file" ] ; then
+# Create the output folder
+if [ -d "$directory/joined_file" ] ; then
 	echo "Can't create folder. Folder already exists."
 	exit 1
 fi
@@ -49,6 +45,8 @@ fi
 
 
 # Heavy lifting
-array=("${directory}/"*."${extension}" ) && 
-	head -1 ${array[0]} > "${directory}/joined_file/result.csv" &&
-       	tail -n +2 -q ${array[@]:0} >> "${directory}/joined_file/result.csv"
+array=( "${directory}/"*."${extension}" ) && 
+	head -1 ${array[0]} > "${directory}/joined_file/${out_file}" &&
+       	tail -n +2 -q ${array[@]:0} >> "${directory}/joined_file/${out_file}"
+
+exit 0
