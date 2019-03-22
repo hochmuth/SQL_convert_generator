4e2f75d9f4b6ae7efddf52b28d3d04e527fb7a59
Daniel Hochmuth <sleepy.locust@gmail.com>
2019-03-22 09:20:17 +0100

    working on it

--- tools/ConcatenateAll.sh
+++ tools/ConcatenateAll.sh
@@ -14,18 +14,39 @@
 
 # Should probably create a new folder to store the newly created file.
 
-
 # Ideas
 # First write the header (first line), then everything else 
-head -1 file.txt > all.txt && tail -n +2 -q >> all.txt
+# head -1 file.txt > all.txt && tail -n +2 -q >> all.txt
 
 # Or
-head -n1 file1.txt > combined.txt
-for fname in *.txt
-do
-	    tail -n+2 $fname >> combined.txt
-done
+# head -n1 file1.txt > combined.txt
+# for fname in *.txt
+# do
+# 	    tail -n+2 $fname >> combined.txt
+# done
 
 # Store all files in an array; write the header from the first one; for all the rest append all expect the header
+
+# Arguments
+extension='csv'
+
+if [[ $# = 0 ]]; then
+	directory="."
+else
+	directory="${1}"
+fi
+
+
+# Create necessary folder
+if [-d "$directory/joined_file" ] ; then
+	echo "Can't create folder. Folder already exists."
+	exit 1
+fi
+
+if [ ! -d "$directory/joined_file" ] ; then
+	mkdir "$directory/joined_file"
+fi
+
+
 
 array=( *.txt );head -1 ${array[0]} > all.txt; tail -n +2 -q ${array[@]:0} >> all.txt
