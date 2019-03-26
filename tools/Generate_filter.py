1de29a77b7ad87a29c11088a95d1c27d2e6fad4d
Daniel Hochmuth <sleepy.locust@gmail.com>
2019-03-26 14:04:53 +0100

    added

---
+++ tools/Generate_filter.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 13:03:54 2019
+
+"""
+
+def generate_filter(in_scope_args, out_file, field='BUKRS'):
+    for arg in in_scope_args:
+        out_file.write('<Filter>\n')
+        out_file.write('<Name>'+field+'</Name>\n')
+        out_file.write('<Mandatory>false</Mandatory>\n')
+        out_file.write('<Sign>I</Sign>\n')
+        out_file.write('<Option>EQ</Option>\n')
+        out_file.write('<Low>'+arg+'</Low>\n')
+        out_file.write('<SmartExporterIgnore>false</SmartExporterIgnore>\n')
+        out_file.write('</Filter>\n')
+        out_file.write('\n')
+
+if __name__ == '__main__':
+    in_scope_ekgrp = []
+    
+    in_scope_ekorg = []
+    
+    out_file = open('filter.txt', 'w', encoding='utf8')
+    generate_filter(in_scope_ekgrp, out_file, field='EKGRP')
+    generate_filter(in_scope_ekorg, out_file, field='EKORG')
+    out_file.close()      
