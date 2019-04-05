ecbca47baef102ffaac4d65ff11c46c4b0bae820
Daniel Hochmuth <sleepy.locust@gmail.com>
2019-04-05 09:23:14 +0200

    csv and log removal

--- tools/CleanUp.sh
+++ tools/CleanUp.sh
@@ -9,10 +9,10 @@ if [ -d "./02_encoding" ] ; then
 	rm -rf ./02_encoding
 fi
 
-if [ -f "./*.csv" ] ; then
-	rm ./*.csv
+if [ -f *.csv ] ; then
+	rm *.csv
 fi
 
-if [ -f "./*.csv" ] ; then
-	rm ./*.log
+if [ -f *.log ] ; then
+	rm *.log
 fi
