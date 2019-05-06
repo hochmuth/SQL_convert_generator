333a669835369ea939d8af34ad06f4c42a6cd90b
Daniel Hochmuth <sleepy.locust@gmail.com>
2019-05-06 17:17:31 +0200

    adds new filters

--- SE_request_generator/SE_request_generator.py
+++ SE_request_generator/SE_request_generator.py
@@ -4,49 +4,41 @@ Created on Sat May  4 17:18:36 2019
 
 @author: ES579DX
 """
-
-import xml.etree.ElementTree as ET
+import lxml.etree as ET
+#import xml.etree.ElementTree as ET
 import copy
+
+BUKRS = ['B001', 'B002', 'B003']
+
+namespaces = {'ns0' : '{http://www.audicon.net/DataRequest}'}
 
 tree = ET.parse('TEST_02.xml')
 root = tree.getroot()
 
+# Look at what we've got
 ET.dump(root)
-
-#print(root.tag)
-#print(root.attrib)
     
-request = root.find('{http://www.audicon.net/DataRequest}Requests')[0]
+request = root.find('{http://www.audicon.net/DataRequest}Requests').find('{http://www.audicon.net/DataRequest}Request')
    
 tables = request.findall('{http://www.audicon.net/DataRequest}Table')
-for table in tables:
-    print(table[0].text, table.tag, type(table))
-    print(table[1].tag)
-    
-for table in tables:
-    if table[1].text == 'Filter':
-        print(table[0].text, table[1].text)
-
-req = root.find('{http://www.audicon.net/DataRequest}Requests')
-ET.dump(req)
-req_inner = req.find('{http://www.audicon.net/DataRequest}Request')
-#print(req_inner)
-ET.dump(req_inner)
 
 copies = []
 for item in root.iter('{http://www.audicon.net/DataRequest}Filter'):
-    ET.dump(item)
-    copied = copy.deepcopy(item)
-    copies.append(copied)
+    print(item.find('..'))
+        
     if item.find('{http://www.audicon.net/DataRequest}Name').text == 'BUKRS':
-        item.find('{http://www.audicon.net/DataRequest}Low').text = 'DDDD'
-    
+        for element in BUKRS:
+            copied = copy.deepcopy(item)
+            copied.find('{http://www.audicon.net/DataRequest}Low').text = element
+#           copies.append(copied)
+            item.addnext(copied)
+        
+
 ET.dump(root)
+   
+#for copied in copies:
+#    ET.dump(copied)
 
-
-    
-    
-    
 
 # TO DO:
-    # Replace filters
+    # Delete the first filter element
