2d796ca18f684269e469fdc35cc00f01e9e86f2a
Daniel Hochmuth <sleepy.locust@gmail.com>
2019-05-07 09:27:43 +0200

    stored filter update in a function

--- SE_request_generator/SE_request_generator.py
+++ SE_request_generator/SE_request_generator.py
@@ -9,6 +9,11 @@ import lxml.etree as ET
 import copy
 
 BUKRS = ['B001', 'B002', 'B003']
+KKBER = ['KK01', 'KK02', 'KK03']
+VKORG = ['V001', 'V002', 'V003']
+GJAHR = ['2017', '2018']
+FILTER_LIST = [BUKRS, KKBER, VKORG, GJAHR]
+FILTER_NAMES = ['BUKRS', 'KKBER', 'VKORG', 'GJAHR']
 
 namespaces = {'ns0' : '{http://www.audicon.net/DataRequest}'}
 ns = '{http://www.audicon.net/DataRequest}'
@@ -24,26 +29,41 @@ print()
 #tables = request.findall(ns+'Table')
 
 
-for item in root.iter(ns+'Filter'):
-#    print(item.find('..'))
-        
-    if item.find(ns+'Name').text == 'BUKRS':
-        for element in BUKRS:
-            copied = copy.deepcopy(item)
-            copied.find(ns+'Low').text = element
-            item.addnext(copied)
-        parent = item.getparent()
-        parent.remove(item)
+#for item in root.iter(ns+'Filter'):
+##    print(item.find('..'))
+#        
+#    if item.find(ns+'Name').text == 'BUKRS':
+#        for element in BUKRS:
+#            copied = copy.deepcopy(item)
+#            copied.find(ns+'Low').text = element
+#            item.addnext(copied)
+#        parent = item.getparent()
+#        parent.remove(item)
         
 
-ET.dump(root)
 
-output_tree = ET.ElementTree(root)
+
+def fill_out_filter(root_tree, filter_list, filter_names):
+    for i, filter_field in enumerate(filter_list):
+        if len(filter_field) > 0:
+            for element in root_tree.iter(ns+'Filter'):
+                if element.find(ns+'Name').text == FILTER_NAMES[i]:
+                    for item in filter_field:
+                        copied = copy.deepcopy(element)
+                        copied.find(ns+'Low').text = item
+                        element.addnext(copied)
+                    parent = element.getparent()
+                    parent.remove(element)
+    return root_tree
+
+
+                    
+output_tree = ET.ElementTree(fill_out_filter(root, FILTER_LIST, FILTER_NAMES))
+            
+
+# Save the result in a file
+#output_tree = ET.ElementTree(root)
 output_tree.write('output.xml', pretty_print=True, xml_declaration=True, encoding="utf-8")
    
-#for copied in copies:
-#    ET.dump(copied)
-
-
 # TO DO:
-    # Delete the first filter element
+    # add VKORG, KKBER, GJAHR
