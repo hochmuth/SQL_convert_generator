cf5a243891eb98c0b885a1eabe61876d76d3f08d
Daniel Hochmuth <sleepy.locust@gmail.com>
2019-05-07 10:05:26 +0200

    added fill_out_dates function

--- SE_request_generator/SE_request_generator.py
+++ SE_request_generator/SE_request_generator.py
@@ -8,6 +8,8 @@ import lxml.etree as ET
 #import xml.etree.ElementTree as ET
 import copy
 
+date_low = '01-01-2018'
+date_high = '12-31-2018'
 BUKRS = ['B001', 'B002', 'B003']
 KKBER = ['KK01', 'KK02', 'KK03']
 VKORG = ['V001', 'V002', 'V003']
@@ -20,28 +22,6 @@ ns = '{http://www.audicon.net/DataRequest}'
 
 tree = ET.parse('TEST_02.xml')
 root = tree.getroot()
-
-# Look at what we've got
-ET.dump(root)
-print()
-    
-#request = root.find(ns+'Requests').find(ns+'Request')   
-#tables = request.findall(ns+'Table')
-
-
-#for item in root.iter(ns+'Filter'):
-##    print(item.find('..'))
-#        
-#    if item.find(ns+'Name').text == 'BUKRS':
-#        for element in BUKRS:
-#            copied = copy.deepcopy(item)
-#            copied.find(ns+'Low').text = element
-#            item.addnext(copied)
-#        parent = item.getparent()
-#        parent.remove(item)
-        
-
-
 
 def fill_out_filter(root_tree, filter_list, filter_names):
     for i, filter_field in enumerate(filter_list):
@@ -56,13 +36,20 @@ def fill_out_filter(root_tree, filter_list, filter_names):
                     parent.remove(element)
     return root_tree
 
+def fill_out_dates(root_tree, date_low, date_high):
+    for tag in root_tree.iter():
+        if (tag.text is not None) and (tag.text.upper() == 'XXXXXXXX'):
+            tag.text = date_low    
+        if  (tag.text is not None) and (tag.text.upper() == 'YYYYYYYY'):
+            tag.text = date_high
+    return root_tree
 
+
+
+tree = ET.parse('TEST_03.xml')
+root = tree.getroot()
                     
-output_tree = ET.ElementTree(fill_out_filter(root, FILTER_LIST, FILTER_NAMES))
-            
-
-# Save the result in a file
-#output_tree = ET.ElementTree(root)
+output_tree = ET.ElementTree(fill_out_filter(fill_out_dates(root, date_low, date_high), FILTER_LIST, FILTER_NAMES))
 output_tree.write('output.xml', pretty_print=True, xml_declaration=True, encoding="utf-8")
    
 # TO DO:
