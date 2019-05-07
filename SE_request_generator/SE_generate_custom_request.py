77363f9118c15d5f3cced06cce62dd858990b1c7
Daniel Hochmuth <sleepy.locust@gmail.com>
2019-05-07 12:23:10 +0200

    appending new cols works

--- SE_request_generator/SE_generate_custom_request.py
+++ SE_request_generator/SE_generate_custom_request.py
@@ -7,6 +7,11 @@ Created on Tue May  7 10:55:49 2019
 import copy
 import lxml.etree as ET
 
+tables_dict = {
+        'BKPF' : ['COL1', 'COL2', 'COL3'],
+        'BSEG' : ['COL1', 'COL2', 'COL3', 'COL4']
+        }
+
 namespaces = {'ns0' : '{http://www.audicon.net/DataRequest}'}
 ns = '{http://www.audicon.net/DataRequest}'
 
@@ -21,12 +26,22 @@ ET.dump(sample_table)
 ET.dump(sample_column)
 ET.dump(sample_filter)
 
+for key in tables_dict:
+    copied_table = copy.deepcopy(sample_table)
+    copied_table.find(ns+'Name').text = key
+    orig_column = copied_table.find(ns+'Column')
+    for item in tables_dict[key]:
+        # Generate a new column
+        copied_column = copy.deepcopy(sample_column)
+        copied_column.find(ns+'Name').text = item
+        # Append the new column        
+        orig_column.addprevious(copied_column)
+    copied_table.remove(orig_column)
+    ET.dump(copied_table)
+
 
 
 '''
     TO DO:
-        From an example SE request:
-            get sample table (empty)
-            get sample column
-            get sample filter
+        Generate two more tables with some sample columns, plus delete the first one.
 '''
