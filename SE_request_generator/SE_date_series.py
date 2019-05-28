284d05bbdc42ae22355207f1f0b0324b75b47cb8
Daniel Hochmuth <sleepy.locust@gmail.com>
2019-05-28 16:08:20 +0200

    creates output dir

--- SE_request_generator/SE_date_series.py
+++ SE_request_generator/SE_date_series.py
@@ -9,11 +9,12 @@ import lxml.etree as ET
 from datetime import datetime, timedelta
 from SE_replace_filters import fill_out_dates
 
-first_date = '20180201'
-last_date = '20180331'
-increment_by = 2
+first_date = '20180101'
+last_date = '20181231'
+increment_by = 5
 
 input_file = 'TEST_03.xml'
+out_directory = 'date_range_output'
 
 # Needs to be a valid date range
 assert datetime.strptime(first_date, '%Y%m%d') < datetime.strptime(last_date, '%Y%m%d')
@@ -22,17 +23,21 @@ assert datetime.strptime(first_date, '%Y%m%d') < datetime.strptime(last_date, '%
 tmp_name = os.path.splitext(input_file)[0]
 fname = os.path.basename(tmp_name)
 
+# Prepare the output directory
+if not os.path.exists('./'+ out_directory + '/'):
+    os.makedirs('./'+ out_directory + '/')
+
 # Main loop
 orig_date = first_date
 while datetime.strptime(orig_date, '%Y%m%d') <= datetime.strptime(last_date, '%Y%m%d'):    
     added_low = orig_date
     added_high = (datetime.strptime(orig_date, '%Y%m%d') + timedelta(days=increment_by)).strftime('%Y%m%d')
     print('Low', added_low)
-    print('High', added_high)    
+    print('High', added_high)  
     orig_date = (datetime.strptime(added_high, '%Y%m%d') + timedelta(days=1)).strftime('%Y%m%d')
     
     tree = ET.parse(input_file)    
     root = tree.getroot()
     
     output_tree = ET.ElementTree(fill_out_dates(root, added_low, added_high))
-    output_tree.write('./date_range_test/' + fname + '_' + added_low + '.xml', pretty_print=True, xml_declaration=True, encoding="utf-8")
+    output_tree.write('./' + out_directory + '/' + fname + '_' + added_low + '.xml', pretty_print=True, xml_declaration=True, encoding="utf-8")
